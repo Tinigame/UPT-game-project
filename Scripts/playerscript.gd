64b11aa32a7e553f05bda9dc63d379c1407c8f27
Tinigame <aidinikandreas@gmail.com>
2025-04-20 20:10:14 +0300

    cleaned up player script

--- Scripts/playerscript.gd
+++ Scripts/playerscript.gd
@@ -6,45 +6,41 @@ extends CharacterBody3D
 const SPEED = 10.0
 const JUMP_VELOCITY = 5
 var MOUSE_SENSITIVITY = 0.002
-var buildmode = false
-
-# Get the gravity from the project settings to be synced with RigidDynamicBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+
 
 func _ready():
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
+
+#Camera and movement rotation
 func _input(event):
-	#Camera rotation
-	if event is InputEventMouseMotion and buildmode == false:
+	if event is InputEventMouseMotion and Globals.buildmode == false:
 		rotate_y(-event.relative.x * MOUSE_SENSITIVITY)
 		camera.rotate_x(-event.relative.y * MOUSE_SENSITIVITY)
 		camera.rotation.x = clamp(camera.rotation.x, -PI/2, PI/2)
 
-func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y -= gravity * delta
 
-	# Handle Jump.
-	if Input.is_action_just_pressed("space") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-	if Input.is_action_just_pressed("exit"):
-		get_tree().quit()
+#toggle between build mode
+func build_toggle():
 	if Input.is_action_just_pressed("switchcam"):
 		if camera.current == true:
 			rotation = Vector3(0, 0, 0)
 			tcamera.rotation_degrees = Vector3(-90, 0, 0)
 			tcamera.current = true
-			buildmode = true
+			Globals.buildmode = true
 			Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 		elif tcamera.current == true:
 			camera.current = true
-			buildmode = false
+			Globals.buildmode = false
 			Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
+#handle jump, movement
+func player_movement():
+	# Handle Jump.
+	if Input.is_action_just_pressed("space") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir = Input.get_vector("A", "D", "W", "S")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
@@ -54,4 +50,16 @@ func _physics_process(delta):
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+
+	#exit the game when esc pressed
+	if Input.is_action_just_pressed("exit"):
+		get_tree().quit()
+
+	player_movement()
+	build_toggle()
 	move_and_slide()
