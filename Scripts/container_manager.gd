479322af14a5c13aa61a56a57e21566f40e3049d
Tinigame <aidinikandreas@gmail.com>
2025-08-08 01:12:40 +0300

    items work yipee

--- Scripts/container_manager.gd
+++ Scripts/container_manager.gd
@@ -1,14 +1,41 @@
 extends Node3D
 
 var max_items = 0
-var items : Dictionary = {}
+var free_slots = 0
+var _content : Array[Item] = []
+
+var debugitem
+
+func _ready() -> void:
+	debugitem = preload("res://Resources/items/conveyor_belt_item.tres")
 
 func set_max_items(new_max_items):
 	max_items = new_max_items
+	free_slots = max_items
 
 
-#TODO todo
-func add_item():
-	return
-func remove_item():
-	return
+func add_item(item:Item):
+	if free_slots > 0:
+		free_slots -= 1
+		_content.append(item)
+	else:
+		return
+
+
+func remove_item(item:Item):
+	_content.erase(item)
+	free_slots += 1
+
+
+func get_items() -> Array[Item]:
+	return _content
+
+
+func debug_populate_containers():
+	add_item(debugitem)
+
+
+func _physics_process(_delta: float) -> void:
+	if Input.is_action_just_pressed("debug_populate_containers"):
+		print("populating containers")
+		debug_populate_containers()
--- Scripts/conveyor_script.gd
+++ Scripts/conveyor_script.gd
@@ -3,15 +3,22 @@ extends Node3D
 var grid_position: Vector3i
 var debugname = "conveyorinator"
 
+var conveyor_contents : Array
+
 #the offset required to see where the forward cell is
 var forward_cell_offset : Vector3i
 
+
+var container_manager
+var container_manager_node
 func _ready() -> void:
-	var container_manager = preload("res://Scenes/Container_manager.tscn")
-	var container_manager_node = container_manager.instantiate()
+	container_manager = preload("res://Scenes/Container_manager.tscn")
+	container_manager_node = container_manager.instantiate()
+	container_manager_node.set_max_items(1)
 	self.add_child(container_manager_node)
 	self.name = debugname
 	add_to_group("conveyors")
+
 
 
 func update_connections():
@@ -22,3 +29,16 @@ func check_neighbor(offset: Vector3i) -> Node3D:
 	if BuildingManager.occupied_cells.has(pos):
 		return BuildingManager.occupied_cells[pos]
 	return null
+
+
+#TODO todo, should visibly display the contents ingame somehow, using the mesh maybe
+func render_contents(contents):
+	for thing in contents:
+		print("the container has a ", thing.item_name, " in it.")
+	return
+
+func _physics_process(_delta: float) -> void:
+	conveyor_contents = container_manager_node.get_items()
+	render_contents(conveyor_contents)
+	
+	
